7c3366a663821c212d344fbae67ab540adff791e
Evgeniy Nekipelov <johnnykayfun@gmail.com>
2025-07-28 23:13:51 +0300

    create project, add layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,308 @@
+import Bar from "./components/Bar/Bar";
+import cls from "./page.module.css";
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className={cls.wrapper}>
+      <div className={cls.container}>
+        <main className={cls.main}>
+          <nav className={cls.main__nav}>
+            <div className={cls.nav__logo}>
+              {/* <Image
+                width={250}
+                height={170}
+                className={"logo__image"}
+                src="/img/logo.png"
+                alt={"logo"}
+              /> */}
+            </div>
+            <div className={cls.nav__burger}>
+              <span className={cls.burger__line}></span>
+              <span className={cls.burger__line}></span>
+              <span className={cls.burger__line}></span>
+            </div>
+            <div className={cls.nav__menu}>
+              <ul className={cls.menu__list}>
+                <li className={cls.menu__item}>
+                  {/*TODO: a -> Link*/}
+                  <a href="#" className={cls.menu__link}>
+                    Главное
+                  </a>
+                </li>
+                <li className={cls.menu__item}>
+                  <a href="#" className={cls.menu__link}>
+                    Мой плейлист
+                  </a>
+                </li>
+                <li className={cls.menu__item}>
+                  <a href="../signin.html" className={cls.menu__link}>
+                    Войти
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <div className={cls.centerblock}>
+            <div className={cls.centerblock__search}>
+              <svg className={cls.search__svg}>
+                <use xlinkHref="/img/icon/sprite.svg#icon-search"></use>
+              </svg>
+              <input
+                className={cls.search__text}
+                type="search"
+                placeholder="Поиск"
+                name="search"
+              />
+            </div>
+            <h2 className={cls.centerblock__h2}>Треки</h2>
+            <div className={cls.centerblock__filter}>
+              <div className={cls.filter__title}>Искать по:</div>
+              <div className={cls.filter__button}>исполнителю</div>
+              <div className={cls.filter__button}>году выпуска</div>
+              <div className={cls.filter__button}>жанру</div>
+            </div>
+            <div className={cls.centerblock__content}>
+              <div className={cls.content__title}>
+                <div className={cls.playlistTitle__col + " " + cls.col01}>
+                  Трек
+                </div>
+                <div className={cls.playlistTitle__col + " " + cls.col02}>
+                  Исполнитель
+                </div>
+                <div className={cls.playlistTitle__col + " " + cls.col03}>
+                  Альбом
+                </div>
+                <div className={cls.playlistTitle__col + " " + cls.col04}>
+                  <svg className={cls.playlistTitle__svg}>
+                    <use xlinkHref="/img/icon/sprite.svg#icon-watch"></use>
+                  </svg>
+                </div>
+              </div>
+              <div className={cls.content__playlist}>
+                <div className={cls.playlist__item}>
+                  <div className={cls.playlist__track}>
+                    <div className={cls.track__title}>
+                      <div className={cls.track__titleImage}>
+                        <svg className={cls.track__titleSvg}>
+                          <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
+                        </svg>
+                      </div>
+                      <div className={cls.track__titleText}>
+                        <a className={cls.track__titleLink} href="">
+                          Guilt <span className={cls.track__titleSpan}></span>
+                        </a>
+                      </div>
+                    </div>
+                    <div className={cls.track__author}>
+                      <a className={cls.track__authorLink} href="">
+                        Nero
+                      </a>
+                    </div>
+                    <div className={cls.track__album}>
+                      <a className={cls.track__albumLink} href="">
+                        Welcome Reality
+                      </a>
+                    </div>
+                    <div className={cls.track__time}>
+                      <svg className={cls.track__timeSvg}>
+                        <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
+                      </svg>
+                      <span className={cls.track__timeText}>4:44</span>
+                    </div>
+                  </div>
+                </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+                <div className={cls.playlist__item}>
+                  <div className={cls.playlist__track}>
+                    <div className={cls.track__title}>
+                      <div className={cls.track__titleImage}>
+                        <svg className={cls.track__titleSvg}>
+                          <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
+                        </svg>
+                      </div>
+                      <div className={cls.track__titleText}>
+                        <a className={cls.track__titleLink} href="">
+                          Elektro <span className={cls.track__titleSpan}></span>
+                        </a>
+                      </div>
+                    </div>
+                    <div className={cls.track__author}>
+                      <a className={cls.track__authorLink} href="">
+                        Dynoro, Outwork, Mr. Gee
+                      </a>
+                    </div>
+                    <div className={cls.track__album}>
+                      <a className={cls.track__albumLink} href="">
+                        Elektro
+                      </a>
+                    </div>
+                    <div className={cls.track__time}>
+                      <svg className={cls.track__timeSvg}>
+                        <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
+                      </svg>
+                      <span className={cls.track__timeText}>2:22</span>
+                    </div>
+                  </div>
+                </div>
+
+                <div className={cls.playlist__item}>
+                  <div className={cls.playlist__track}>
+                    <div className={cls.track__title}>
+                      <div className={cls.track__titleImage}>
+                        <svg className={cls.track__titleSvg}>
+                          <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
+                        </svg>
+                      </div>
+                      <div className={cls.track__titleText}>
+                        <a className={cls.track__titleLink} href="">
+                          I’m Fire{" "}
+                          <span className={cls.track__titleSpan}></span>
+                        </a>
+                      </div>
+                    </div>
+                    <div className={cls.track__author}>
+                      <a className={cls.track__authorLink} href="">
+                        Ali Bakgor
+                      </a>
+                    </div>
+                    <div className={cls.track__album}>
+                      <a className={cls.track__albumLink} href="">
+                        I’m Fire
+                      </a>
+                    </div>
+                    <div className={cls.track__time}>
+                      <svg className={cls.track__timeSvg}>
+                        <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
+                      </svg>
+                      <span className={cls.track__timeText}>2:22</span>
+                    </div>
+                  </div>
+                </div>
+
+                <div className={cls.playlist__item}>
+                  <div className={cls.playlist__track}>
+                    <div className={cls.track__title}>
+                      <div className={cls.track__titleImage}>
+                        <svg className={cls.track__titleSvg}>
+                          <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
+                        </svg>
+                      </div>
+                      <div className={cls.track__titleText}>
+                        <a className={cls.track__titleLink} href="">
+                          Non Stop
+                          <span className={cls.track__titleSpan}>(Remix)</span>
+                        </a>
+                      </div>
+                    </div>
+                    <div className={cls.track__author}>
+                      <a className={cls.track__authorLink} href="">
+                        Стоункат, Psychopath
+                      </a>
+                    </div>
+                    <div className={cls.track__album}>
+                      <a className={cls.track__albumLink} href="">
+                        Non Stop
+                      </a>
+                    </div>
+                    <div className={cls.track__time}>
+                      <svg className={cls.track__timeSvg}>
+                        <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
+                      </svg>
+                      <span className={cls.track__timeText}>4:12</span>
+                    </div>
+                  </div>
+                </div>
+
+                <div className={cls.playlist__item}>
+                  <div className={cls.playlist__track}>
+                    <div className={cls.track__title}>
+                      <div className={cls.track__titleImage}>
+                        <svg className={cls.track__titleSvg}>
+                          <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
+                        </svg>
+                      </div>
+                      <div>
+                        <a className={cls.track__titleLink} href="">
+                          Run Run
+                          <span className={cls.track__titleSpan}>
+                            (feat. AR/CO)
+                          </span>
+                        </a>
+                      </div>
+                    </div>
+                    <div className={cls.track__author}>
+                      <a className={cls.track__authorLink} href="">
+                        Jaded, Will Clarke, AR/CO
+                      </a>
+                    </div>
+                    <div className={cls.track__album}>
+                      <a className={cls.track__albumLink} href="">
+                        Run Run
+                      </a>
+                    </div>
+                    <div className={cls.track__time}>
+                      <svg className={cls.track__timeSvg}>
+                        <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
+                      </svg>
+                      <span className={cls.track__timeText}>2:54</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={cls.main__sidebar}>
+            <div className={cls.sidebar__personal}>
+              <p className={cls.sidebar__personalName}>Sergey.Ivanov</p>
+              <div className={cls.sidebar__icon}>
+                <svg>
+                  <use xlinkHref="/img/icon/sprite.svg#logout"></use>
+                </svg>
+              </div>
+            </div>
+            <div className={cls.sidebar__block}>
+              <div className={cls.sidebar__list}>
+                <div className={cls.sidebar__item}>
+                  <a className={cls.sidebar__link} href="#">
+                    <Image
+                      className={cls.sidebar__img}
+                      src="/img/playlist01.png"
+                      alt="day's playlist"
+                      width={250}
+                      height={170}
+                    />
+                  </a>
+                </div>
+                <div className={cls.sidebar__item}>
+                  <a className={cls.sidebar__link} href="#">
+                    <Image
+                      className={cls.sidebar__img}
+                      src="/img/playlist02.png"
+                      alt="day's playlist"
+                      width={250}
+                      height={170}
+                    />
+                  </a>
+                </div>
+                <div className={cls.sidebar__item}>
+                  <a className={cls.sidebar__link} href="#">
+                    <Image
+                      className={cls.sidebar__img}
+                      src="/img/playlist03.png"
+                      alt="day's playlist"
+                      width={250}
+                      height={170}
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        <Bar />
+        <footer className="footer"></footer>
+      </div>
     </div>
   );
 }
---
+++ src/components/Bar/Bar.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import cls from "./Bar.module.css";
+
+const Bar = () => {
+  return (
+    <div className={cls.bar}>
+      <div className={cls.bar__content}>
+        <div className={cls.bar__playerProgress}></div>
+        <div className={cls.bar__playerBlock}>
+          <div className={cls.bar__player}>
+            <div className={cls.player__controls}>
+              <div className={cls.player__btnPrev}>
+                <svg className={cls.player__btnPrevSvg}>
+                  <use xlinkHref="/img/icon/sprite.svg#icon-prev"></use>
+                </svg>
+              </div>
+              <div className={`${cls.player__btnPlay} ${cls.btn}`}>
+                <svg className={cls.player__btnPlaySvg}>
+                  <use xlinkHref="/img/icon/sprite.svg#icon-play"></use>
+                </svg>
+              </div>
+              <div className={cls.player__btnNext}>
+                <svg className={cls.player__btnNextSvg}>
+                  <use xlinkHref="/img/icon/sprite.svg#icon-next"></use>
+                </svg>
+              </div>
+              <div className={`${cls.player__btnRepeat} ${cls.btnIcon}`}>
+                <svg className={cls.player__btnRepeatSvg}>
+                  <use xlinkHref="/img/icon/sprite.svg#icon-repeat"></use>
+                </svg>
+              </div>
+              <div className={`${cls.player__btnShuffle} ${cls.btnIcon}`}>
+                <svg className={cls.player__btnShuffleSvg}>
+                  <use xlinkHref="/img/icon/sprite.svg#icon-shuffle"></use>
+                </svg>
+              </div>
+            </div>
+
+            <div className={cls.player__trackPlay}>
+              <div className={cls.trackPlay__contain}>
+                <div className={cls.trackPlay__image}>
+                  <svg className={cls.trackPlay__svg}>
+                    <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
+                  </svg>
+                </div>
+                <div className={cls.trackPlay__author}>
+                  <a className={cls.trackPlay__authorLink} href="">
+                    Ты та...
+                  </a>
+                </div>
+                <div className={cls.trackPlay__album}>
+                  <a className={cls.trackPlay__albumLink} href="">
+                    Баста
+                  </a>
+                </div>
+              </div>
+
+              <div className={cls.trackPlay__dislike}>
+                <div className={`${cls.player__btnShuffle} ${cls.btnIcon}`}>
+                  <svg className={cls.trackPlay__likeSvg}>
+                    <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
+                  </svg>
+                </div>
+                <div className={`${cls.trackPlay__dislike} ${cls.btnIcon}`}>
+                  <svg className={cls.trackPlay__dislikeSvg}>
+                    <use xlinkHref="/img/icon/sprite.svg#icon-dislike"></use>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={cls.bar__volumeBlock}>
+            <div className={cls.volume__content}>
+              <div className={cls.volume__image}>
+                <svg className={cls.volume__svg}>
+                  <use xlinkHref="/img/icon/sprite.svg#icon-volume"></use>
+                </svg>
+              </div>
+              <div className={`${cls.volume__progress} ${cls.btn}`}>
+                <input
+                  className={`${cls.volume__progressLine} ${cls.btn}`}
+                  type="range"
+                  name="range"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Bar;
