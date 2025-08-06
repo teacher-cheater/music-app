fa9b99d128bc7f16ce3526c05f2ece457de4c8c4
Evgeniy Nekipelov <johnnykayfun@gmail.com>
2025-08-07 00:32:28 +0300

    add component navigation

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import cls from "./main.module.css";
-import Link from "next/link";
 import Centerblock from "../Centerblock/Centerblock";
-import Sidebar from "../Sidebar/Sidebar";
 import { getTracks } from "@/services/tracks/tracksApi";
 import { useEffect, useState } from "react";
 import { TrackType } from "@/sharedtypes/sharedTypes";
@@ -38,43 +36,7 @@ const Main = () => {
 
   return (
     <main className={cls.main}>
-      <nav className={cls.main__nav}>
-        <div className={cls.nav__logo}>
-          {/* <Image
-            width={250}
-            height={170}
-            className={"logo__image"}
-            src="/img/logo.png"
-            alt={"logo"}
-          /> */}
-        </div>
-        <div className={cls.nav__burger}>
-          <span className={cls.burger__line}></span>
-          <span className={cls.burger__line}></span>
-          <span className={cls.burger__line}></span>
-        </div>
-        <div className={cls.nav__menu}>
-          <ul className={cls.menu__list}>
-            <li className={cls.menu__item}>
-              <Link href="#" className={cls.menu__link}>
-                Главное
-              </Link>
-            </li>
-            <li className={cls.menu__item}>
-              <Link href="#" className={cls.menu__link}>
-                Мой плейлист
-              </Link>
-            </li>
-            <li className={cls.menu__item}>
-              <Link href="../signin.html" className={cls.menu__link}>
-                Войти
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
       <Centerblock allTracks={allTracks} isLoading={isLoading} />
-      <Sidebar />
     </main>
   );
 };
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import cls from "./navigation.module.css";
+
+export default function Navigation() {
+  return (
+    <nav className={cls.main__nav}>
+      <div className={cls.nav__logo}>
+        {/* <Image
+          width={250}
+          height={170}
+          className={"logo__image"}
+          src="/img/logo.png"
+          alt={"logo"}
+        /> */}
+      </div>
+      <div className={cls.nav__burger}>
+        <span className={cls.burger__line}></span>
+        <span className={cls.burger__line}></span>
+        <span className={cls.burger__line}></span>
+      </div>
+      <div className={cls.nav__menu}>
+        <ul className={cls.menu__list}>
+          <li className={cls.menu__item}>
+            <Link href="#" className={cls.menu__link}>
+              Главное
+            </Link>
+          </li>
+          <li className={cls.menu__item}>
+            <Link href="#" className={cls.menu__link}>
+              Мой плейлист
+            </Link>
+          </li>
+          <li className={cls.menu__item}>
+            <Link href="../signin.html" className={cls.menu__link}>
+              Войти
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
