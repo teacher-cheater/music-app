a015675b0baf06c27f39f486a90a7ef175d0a06e
Evgeniy Nekipelov <johnnykayfun@gmail.com>
2025-08-25 20:17:18 +0300

    add logout

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,11 @@
+"use client";
 import Link from "next/link";
 import cls from "./sidebar.module.css";
 import Image from "next/image";
 import Username from "../Username/Username";
+import { useAppDispatch } from "@/store/store";
+import { clearUser } from "@/store/features/authSlice";
+import { useRouter } from "next/navigation";
 
 export default function Sidebar() {
   interface Playlist {
@@ -9,17 +13,25 @@ export default function Sidebar() {
     img: string;
   }
 
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+
   const playlist: Playlist[] = [
     { id: 1, img: "/img/playlist01.png" },
     { id: 2, img: "/img/playlist02.png" },
     { id: 3, img: "/img/playlist03.png" },
   ];
 
+  const logout = async () => {
+    router.push("/auth/signin");
+    dispatch(clearUser());
+  };
+
   return (
     <div className={cls.main__sidebar}>
       <div className={cls.sidebar__personal}>
         <Username />
-        <div className={cls.sidebar__icon}>
+        <div className={cls.sidebar__icon} onClick={() => logout()}>
           <svg>
             <use xlinkHref="/img/icon/sprite.svg#logout" />
           </svg>
